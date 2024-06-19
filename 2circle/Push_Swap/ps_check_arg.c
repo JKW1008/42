728413f5d9ee94a051f865c8b594fbf0d6524214
Kangwoo Jung <kjung@c2r17s3.42gyeongsan.kr>
2024-06-19 16:47:56 +0900

    test

---
+++ 2circle/Push_Swap/ps_check_arg.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_check_arg.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kjung <kjung@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/17 15:25:42 by kjung             #+#    #+#             */
+/*   Updated: 2024/06/19 14:10:02 by kjung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	check_arguments_for_char(t_stack *stack, char *argv)
+{
+	long long		result;
+	char			**divided;
+	int				i;
+	int				j;
+
+	divided = ft_split(argv, ' ');
+	i = 0;
+	while (divided[i] != NULL)
+	{
+		j = 0;
+		while (divided[i][j] != '\0')
+		{
+			if (!(divided[i][j] > '0' && divided[i][j] < '9'))
+				return (0);
+			j++;
+		}
+		result = ft_atol(divided[i]);
+		if (result < INT_MIN || result > INT_MAX)
+			return (0);
+		i++;
+	}
+	parcing_list_for_char(stack, divided);
+	free_split(divided);
+	return (1);
+}
+
+int	check_arguments_for_digit(t_stack *stack, char **argv)
+{
+	long long	result;
+	int			i;
+	int			j;
+
+	i = 1;
+	while (argv[i] != NULL)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+				return (0);
+			j++;
+		}
+		result = ft_atol(argv[i]);
+		if (result < INT_MIN || result > INT_MAX)
+			return (0);
+		i++;
+	}
+	parcing_list_for_digit(stack, argv);
+	return (1);
+}
+
+int	check_arg(int argc, char **argv, t_stack *stack)
+{
+	if (argc < 2)
+	{
+		ft_printf("Error\n");
+		return (0);
+	}
+	else if (argc == 2)
+	{
+		if (check_arguments_for_char(stack, argv[1]) == 0)
+		{
+			ft_printf("Error\n");
+			return (0);
+		}
+	}
+	else
+	{
+		if (check_arguments_for_digit(stack, argv) == 0)
+		{
+			ft_printf("Error\n");
+			return (0);
+		}
+	}
+	return (1);
+}
---
+++ 2circle/Push_Swap/ps_list_parcing.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_list_parcing.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kjung <kjung@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/17 16:32:33 by kjung             #+#    #+#             */
+/*   Updated: 2024/06/19 14:07:31 by kjung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void parcing_list_for_char(t_stack *stack, char **devided)
+{
+	t_lst *next;
+	long long result;
+	int i = 0;
+
+	while (devided[i] != NULL)
+	{
+		next = (t_lst *)malloc(sizeof(t_lst));
+		result = ft_atol(devided[i]);
+		next->index = i;
+		next->data = (int)result;
+		next->next = NULL;
+		ft_lstadd_ps_back(stack, next);
+		i++;
+	}
+}
+
+void	parcing_list_for_digit(t_stack *stack, char **argv)
+{
+	t_lst *next;
+	long long result;
+	int i = 1;
+
+	while (argv[i] != NULL)
+	{
+		next = (t_lst *)malloc(sizeof(t_lst));
+		result = ft_atol(argv[i]);
+		next->index = i - 1;
+		next->data = (int)result;
+		next->next = NULL;
+		ft_lstadd_ps_back(stack, next);
+		i++;
+	}
+}
